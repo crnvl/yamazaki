deb395627070a148034eb1cb33a56fbd93b5ca8a
shinixsensei <crezoxplays@gmail.com>
2020-11-17 14:40:43 +0100

    First Login Model

--- src/main/java/Core/Main.java
+++ src/main/java/Core/Main.java
@@ -1,8 +1,31 @@
 package Core;
 
+import Database.Config;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static JDABuilder builder;
+
+    public static void main(String[] args) throws LoginException, IOException {
+
+        Config.init();
+
+        builder = new JDABuilder(AccountType.BOT);
+
+        builder.setStatus(OnlineStatus.IDLE);
+        builder.setAutoReconnect(true);
+        builder.setActivity(Activity.playing("CARRION"));
+
+        builder.setToken(Config.load("token"));
+
+        builder.build();
 
     }
 
---
+++ src/main/java/Database/Config.java
@@ -0,0 +1,63 @@
+package Database;
+
+import java.io.*;
+import java.util.Properties;
+
+public class Config {
+
+    private static Properties properties;
+    static File file = new File("config.properties");
+
+    public static void init() throws IOException {
+        properties = new Properties();
+        try {
+            InputStream in = new FileInputStream(file);
+            properties.load(in);
+            //in.close();
+            //load();
+        } catch (IOException e) {
+            System.out.println("Settings File '" + file.getName() + "' (" + file.getAbsolutePath() + ") not found\n" +
+                                "Please restart!");
+            fileNotFoundAction(file);
+            System.exit(0);
+        }
+    }
+
+    public static boolean propExist(String key) {
+        return properties.getProperty(key) != null;
+    }
+
+    public static String load(String key) {
+        return properties.getProperty(key);
+    }
+
+    public static void save(String hash, String name) {
+        properties.put(hash, name);
+        try {
+            properties.store(new FileOutputStream(file), "Yamazaki Configuration");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void delete(String hash) {
+        properties.remove(hash);
+    }
+
+    private static void fileNotFoundAction(File f){
+        try {
+            properties.store(new FileOutputStream(f), "Yamazaki Configuration");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int getKeySize() {
+        return properties.size();
+    }
+
+    public static void getKeyInt(int i) {
+        properties.get(i);
+    }
+
+}
